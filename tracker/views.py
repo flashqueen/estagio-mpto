48bb3395137c185ee88b25513b4b67d7f6e1b18e
Danilo Oliveira <dohkoweeds74@gmail.com>
2021-04-30 14:06:05 -0400

    foi implementado o endpoint para lidar com pessoas fisicas
    closes #25

--- tracker/views.py
+++ tracker/views.py
@@ -1,7 +1,12 @@
 from helpers import restfy
-from .serializers import StateSerializer, CitySerializer
+from .serializers import (
+    StateSerializer,
+    CitySerializer,
+    NaturalPersonSerializer
+)
 
 
 
+natural_person_index, natural_person_by_id = restfy.make_rest(NaturalPersonSerializer)
 state_index, state_by_id = restfy.make_rest(StateSerializer)
 city_index, city_by_id = restfy.make_rest(CitySerializer)
